df0dce26af68354251ebf4e62f6b788214d90d7e
yanqin24 <156973851+yanqin24@users.noreply.github.com>
2026-02-09 01:00:15 -0800

    Update solution.py

--- greedy/0045-jump-game-ii/solution.py
+++ greedy/0045-jump-game-ii/solution.py
@@ -1,8 +1,14 @@
 class Solution:
-    def canJump(self, nums):
-        farthest = 0
-        for i, step in enumerate(nums):
-            if farthest < i:
-                return False
-            farthest = max(farthest, i + step)
-        return True
+    def jump(self, nums: List[int]) -> int:
+        n=len(nums)
+        fastmove=0
+        cur_end=0
+        jump=0
+        for i in range(n-1):
+            fastmove=max(fastmove,i+nums[i])
+            if cur_end==i:
+                jump+=1
+                cur_end=fastmove
+        return jump
+
+
